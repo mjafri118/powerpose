8ced88ce291bd681ea662c93d9c9a17f384e46dd
Mohib Jafri <mjafri@Mohibs-MacBook-Pro.local>
2018-04-15 11:23:12 -0400

    Heroku attempt without name main

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, flash, redirect, render_template, request, session
+from flask import Flask
 
 
 app= Flask(__name__)
@@ -8,16 +8,16 @@ app= Flask(__name__)
 # https://progblog.io/How-to-deploy-a-Flask-App-to-Heroku/
 
 #Ensure responses aren't cached
-@app.after_request
-def after_request(response):
-    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    response.headers["Expires"] = 0
-    response.headers["Pragma"] = "no-cache"
-    return response
+# @app.after_request
+# def after_request(response):
+#     response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+#     response.headers["Expires"] = 0
+#     response.headers["Pragma"] = "no-cache"
+#     return response
 
 @app.route('/')
 def index():
-    return "I'ts working!"
+    return "<h1>I'ts working!</h1>"
 
 @app.route('/api', methods=['GET', 'POST'])
 def api():
